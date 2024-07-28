2a014e975441ad9d126a454f0dd2af643fdb4946
kannoth <kannoth1997house@gmail.com>
2024-07-28 23:47:24 +0530

    commit3

---
+++ 11_ContainerWithMostWater.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def maxArea(self,height):
+        max_area=0
+        left=0
+        right=len(height)-1
+        while left<right:
+            breadth=right-left
+            length=min(height[left],height[right])
+            area=breadth*length
+            max_area=max(max_area,area)
+            
+            # Move the pointer corresponding to the smaller line
+            if height[left]<height[right]:
+                left=left+1
+            else:
+                right=right-1
+                
+        return max_area
+    
+height = [1,8,6,2,5,4,8,3,7]
+sol=Solution()
+print('max_area',sol.maxArea(height))
